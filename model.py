fe7ebca4bc99f1c841bca28d6d4f35c99a30ea93
Manuja100 <manujamunasinghe1@gmail.com>
2023-11-12 00:51:04 +0530

    initial commit model

--- model.py
+++ model.py
@@ -5,4 +5,14 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 #import the preprocessed dataset  file
-trafficData = pd.read_csv('datasets/traffic_preprocessed.csv')
+df = pd.read_csv('datasets/traffic_preprocessed.csv')
+
+#split to input and output
+x = df.drop('Traffic_Level', axis = 1)
+y = df['Traffic_Level']
+
+#using SMOTEEN to balance the data
+#it is a mix of over-sampling and under-sampling
+from imblearn.combine import SMOTEENN
+smote = SMOTEENN(random_state=42)
+x_smoteen, y_smoteen = smote.fit_resample(x, y)
