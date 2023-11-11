f20049417c281600a8dac37a5df92e06765b32f2
Manuja100 <manujamunasinghe1@gmail.com>
2023-11-12 00:53:55 +0530

    completed model - random forest

--- model.py
+++ model.py
@@ -16,3 +16,13 @@ y = df['Traffic_Level']
 from imblearn.combine import SMOTEENN
 smote = SMOTEENN(random_state=42)
 x_smoteen, y_smoteen = smote.fit_resample(x, y)
+
+#split to train and test data
+X_train, X_test, y_train, y_test = train_test_split(x_smoteen, y_smoteen, test_size = 0.2, shuffle = True)
+
+#training the random forest classifier by tuning hyperparameters
+random_forest = RandomForestClassifier(n_estimators=15, max_features = 'log2', max_depth = 15)
+random_forest.fit(X_train, y_train)
+
+#save the model for future use
+pickle.dump(random_forest, open('model.pkl', 'wb'))
